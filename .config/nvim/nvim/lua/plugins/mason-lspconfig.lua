347f3be01dc455468c86382437a61b77aea50647
SUBHAM-ROY <subham.roy@workday.com>
2025-06-27 19:26:35 -0700

    added config back

---
+++ .config/nvim/nvim/lua/plugins/mason-lspconfig.lua
@@ -0,0 +1,36 @@
+return {
+	"mason-org/mason-lspconfig.nvim",
+	lazy = false,
+	opts = {
+		ensure_installed = {
+			"ts_ls",
+			"pyright",
+			"eslint",
+		},
+	},
+	dependencies = {
+		{ "mason-org/mason.nvim", opts = {} },
+		"neovim/nvim-lspconfig",
+	},
+	keys = {
+		{ "<leader>M", "<cmd>Mason<cr>", desc = "Mason" },
+	},
+	init = function()
+		local icons = {
+			[vim.diagnostic.severity.ERROR] = "",
+			[vim.diagnostic.severity.WARN] = "",
+			[vim.diagnostic.severity.INFO] = "",
+			[vim.diagnostic.severity.HINT] = "󰌶",
+		}
+		vim.diagnostic.config({
+			virtual_text = {
+				prefix = function(diagnostic, _, _)
+					return icons[diagnostic.severity] .. " "
+				end,
+			},
+			signs = {
+				text = icons,
+			},
+		})
+	end,
+}
