825d652398011edff41845d07a43646843bde46b
SUBHAM-ROY <subham.roy@workday.com>
2025-07-01 17:48:49 -0700

    Gruvbox material nvim

---
+++ nvim/.config/nvim/lua/plugins/gruvbox-material.lua
@@ -0,0 +1,11 @@
+return {
+	"sainnhe/gruvbox-material",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		vim.g.gruvbox_material_enable_italic = true
+		vim.g.gruvbox_material_diagnostic_virtual_text = "colored"
+		vim.cmd.colorscheme("gruvbox-material")
+		vim.api.nvim_set_hl(0, "LineNr", { fg = "#665c54", bg = "#1d2021" })
+	end,
+}
