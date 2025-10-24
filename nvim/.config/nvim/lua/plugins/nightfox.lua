09c07534340d521cbdb78e18cd8e9f3326ff4f61
SUBHAM-ROY <subham.roy@workday.com>
2025-10-24 13:34:50 -0700

    back to gruvbox

--- nvim/.config/nvim/lua/plugins/nightfox.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'EdenEast/nightfox.nvim',
-  event = 'VeryLazy',
-  priority = 1000,
-  config = function()
-    vim.cmd.colorscheme('nordfox')
-    vim.api.nvim_set_hl(0, 'Visual', { bg = '#7686ad' })
-  end,
-}
