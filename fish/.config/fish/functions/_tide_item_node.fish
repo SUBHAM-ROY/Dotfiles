780bc663e309a62511dc2d31ab0ea1b56315bcdc
SUBHAM-ROY <subham.roy@workday.com>
2025-10-24 11:56:21 -0700

    delete gitignores

--- fish/.config/fish/functions/_tide_item_node.fish
+++
@@ -1,7 +0,0 @@
-function _tide_item_node
-    #check if current directory is a node project
-    if test -f package.json -o -d node_modules
-        set -l v (node -v 2>/dev/null)
-        _tide_print_item node $tide_node_icon' ' $v
-    end
-end
