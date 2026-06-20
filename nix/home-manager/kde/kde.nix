f80ec3ca10651c8bb41c1e4f9962a006dba4aa9c
Subham Roy <subhamroy.10000@gmail.com>
2026-06-19 23:26:04 -0700

    working widgets and wallpapers

--- nix/home-manager/kde/kde.nix
+++ nix/home-manager/kde/kde.nix
@@ -1,4 +1,9 @@
-{ pkgs, lib, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 
 {
   home.packages = with pkgs; [
@@ -20,6 +25,11 @@
         library = "org.kde.kwin.aurorae";
         theme = "__aurorae__svg__Sweet-Dark";
       };
+      wallpaper = ./office-lofi.jpg;
+    };
+
+    kscreenlocker = {
+      appearance.wallpaper = ./office-lofi.jpg;
     };
 
     kwin.virtualDesktops.number = 5;
@@ -80,6 +90,234 @@
         ];
       }
     ];
+
+    desktop = {
+      widgets = [
+        {
+          name = "org.kde.plasma.analogclock";
+          position = {
+            horizontal = 1472;
+            vertical = 0;
+          };
+          size = {
+            width = 368;
+            height = 272;
+          };
+          config.General.showSecondHand = true;
+        }
+        {
+          systemMonitor = {
+            position = {
+              horizontal = 1376;
+              vertical = 288;
+            };
+            size = {
+              width = 272;
+              height = 240;
+            };
+            displayStyle = "org.kde.ksysguard.linechart";
+            showTitle = false;
+            sensors = [
+              {
+                name = "cpu/all/usage";
+                color = "207,0,0";
+                label = "CPU Usage";
+              }
+            ];
+            textOnlySensors = [ "cpu/all/averageTemperature" ];
+            settings = {
+              Sensors.highPrioritySensorIds = "[\"cpu/all/usage\"]";
+              Sensors.lowPrioritySensorIds = "[\"cpu/all/averageTemperature\"]";
+              Appearance.updateRateLimit = 1000;
+              SensorColors."cpu/all/averageTemperature" = "100,0,0";
+              SensorLabels."cpu/all/averageTemperature" = "CPU Temp";
+            };
+          };
+        }
+        {
+          systemMonitor = {
+            position = {
+              horizontal = 1648;
+              vertical = 288;
+            };
+            size = {
+              width = 272;
+              height = 240;
+            };
+            displayStyle = "org.kde.ksysguard.linechart";
+            showTitle = false;
+            sensors = [
+              {
+                name = "gpu/all/usage";
+                color = "0,0,255";
+                label = "GPU Usage";
+              }
+            ];
+            textOnlySensors = [ "gpu/gpu0/temperature" ];
+            settings = {
+              Sensors.highPrioritySensorIds = "[\"gpu/all/usage\"]";
+              Sensors.lowPrioritySensorIds = "[\"gpu/gpu0/temperature\"]";
+              Appearance.updateRateLimit = 1000;
+              SensorColors."gpu/gpu0/temperature" = "95,76,28";
+              SensorLabels."gpu/gpu0/temperature" = "GPU Temp";
+            };
+          };
+        }
+        {
+          systemMonitor = {
+            position = {
+              horizontal = 1376;
+              vertical = 512;
+            };
+            size = {
+              width = 272;
+              height = 240;
+            };
+            displayStyle = "org.kde.ksysguard.piechart";
+            showTitle = true;
+            title = "Disk Usage";
+            sensors = [
+              {
+                name = "disk/f57a50c1-7903-4f68-91c6-dedcad22a611/free";
+                color = "0,66,0";
+                label = "Free";
+              }
+              {
+                name = "disk/f57a50c1-7903-4f68-91c6-dedcad22a611/used";
+                color = "85,0,0";
+                label = "Used";
+              }
+            ];
+            totalSensors = [ "disk/f57a50c1-7903-4f68-91c6-dedcad22a611/total" ];
+            settings = {
+              Sensors.highPrioritySensorIds = "[\"disk/f57a50c1-7903-4f68-91c6-dedcad22a611/free\", \"disk/f57a50c1-7903-4f68-91c6-dedcad22a611/used\"]";
+              Sensors.totalSensors = "[\"disk/f57a50c1-7903-4f68-91c6-dedcad22a611/total\"]";
+              Appearance.updateRateLimit = 3000;
+              SensorLabels."disk/f57a50c1-7903-4f68-91c6-dedcad22a611/freePercent" = "Free %";
+              SensorLabels."disk/f57a50c1-7903-4f68-91c6-dedcad22a611/usedPercent" = "Used %";
+              SensorLabels."disk/f57a50c1-7903-4f68-91c6-dedcad22a611/total" = "Total";
+              SensorLabels."disk/nvme0n1/total" = "Total";
+            };
+          };
+        }
+        {
+          systemMonitor = {
+            position = {
+              horizontal = 1648;
+              vertical = 512;
+            };
+            size = {
+              width = 272;
+              height = 240;
+            };
+            displayStyle = "org.kde.ksysguard.textonly";
+            showTitle = true;
+            title = "Battery Stats";
+            sensors = [
+              {
+                name = "power/battery_BAT1/chargePercentage";
+                color = "49,151,167";
+                label = "Charge";
+              }
+              {
+                name = "power/battery_BAT1/capacity";
+                color = "95,92,28";
+                label = "Current Cap";
+              }
+              {
+                name = "power/battery_BAT1/design";
+                color = "39,118,0";
+                label = "Design Cap";
+              }
+              {
+                name = "power/battery_BAT1/health";
+                color = "95,46,28";
+                label = "Health";
+              }
+              {
+                name = "power/battery_BAT1/chargeRate";
+                color = "28,57,95";
+                label = "Rate";
+              }
+            ];
+            settings = {
+              Sensors.highPrioritySensorIds = "[\"power/battery_BAT1/chargePercentage\", \"power/battery_BAT1/capacity\", \"power/battery_BAT1/design\", \"power/battery_BAT1/health\", \"power/battery_BAT1/chargeRate\"]";
+              Appearance.updateRateLimit = 3000;
+            };
+          };
+        }
+        {
+          systemMonitor = {
+            position = {
+              horizontal = 1376;
+              vertical = 752;
+            };
+            size = {
+              width = 272;
+              height = 240;
+            };
+            displayStyle = "org.kde.ksysguard.linechart";
+            showTitle = false;
+            sensors = [
+              {
+                name = "memory/physical/used";
+                color = "248,161,0";
+                label = "RAM Usage";
+              }
+            ];
+            textOnlySensors = [
+              "memory/physical/total"
+              "memory/physical/usedPercent"
+            ];
+            settings = {
+              Sensors.highPrioritySensorIds = "[\"memory/physical/used\"]";
+              Sensors.lowPrioritySensorIds = "[\"memory/physical/total\", \"memory/physical/usedPercent\"]";
+              SensorColors."memory/physical/total" = "95,28,79";
+              SensorColors."memory/physical/usedPercent" = "28,95,79";
+              SensorLabels."memory/physical/total" = "RAM Size";
+              SensorLabels."memory/physical/usedPercent" = "RAM %";
+            };
+          };
+        }
+        {
+          systemMonitor = {
+            position = {
+              horizontal = 1648;
+              vertical = 752;
+            };
+            size = {
+              width = 272;
+              height = 240;
+            };
+            displayStyle = "org.kde.ksysguard.linechart";
+            showTitle = false;
+            sensors = [
+              {
+                name = "network/wlo1/download";
+                color = "123,123,185";
+                label = "Downstream";
+              }
+              {
+                name = "network/wlo1/upload";
+                color = "213,204,101";
+                label = "Upstream";
+              }
+            ];
+            textOnlySensors = [
+              "network/wlo1/signal"
+              "network/wlo1/ipv4address"
+            ];
+            settings = {
+              Sensors.highPrioritySensorIds = "[\"network/wlo1/download\", \"network/wlo1/upload\"]";
+              Sensors.lowPrioritySensorIds = "[\"network/wlo1/signal\", \"network/wlo1/ipv4address\"]";
+              Appearance.updateRateLimit = 1000;
+              SensorLabels."network/wlo1/signal" = "Signal";
+              SensorLabels."network/wlo1/ipv4address" = "IPv4";
+            };
+          };
+        }
+      ];
+    };
 
   };
 }
